d36fe182a22c4a3ccba8b46874ea5d33caa9d025
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-19 23:54:33 +0000

    calculations total and avg revenue

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,14 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Exercise 3: Delete the third store
+# 1. Load the third store (into `@store3`) as you did the other two before.
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+
+# @store3 = Store.third
+@store3 = Store.find(id = 3)
+@store3.destroy
+
+puts @store3.name
+puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,39 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Exercise 6: One-to-many association
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's 
+# column structure, you'll find that it has a `store_id` (integer) column. This is a column that 
+# identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create 
+# stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making 
+#   multiple calls to create (like you have before.) No need to assign the employees to variables 
+#   though. Create them through the `@store#` instance variables that you defined in previous exercises. 
+#   Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
+# burnaby = Store.create(
+#   name: "Burnaby", 
+#   annual_revenue: 300000, 
+#   mens_apparel: true,
+#   womens_apparel: true)
+
+@store4 = Store.find(4)
+@store5 = Store.find(5)
+@store6 = Store.find(6)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Dimitrios", last_name: "Petropoulos", hourly_rate: 100)
+@store2.employees.create(first_name: "Jennifer", last_name: "Hsueh", hourly_rate: 90)
+@store4.employees.create(first_name: "Sasha", last_name: "Rhodes", hourly_rate: 70)
+@store4.employees.create(first_name: "Han", last_name: "Tong", hourly_rate: 40)
+@store5.employees.create(first_name: "Stevia", last_name: "Hatzi", hourly_rate: 50)
+@store6.employees.create(first_name: "Jennifer", last_name: "Hass", hourly_rate: 100)
+@store6.employees.create(first_name: "Anna", last_name: "Bach", hourly_rate: 80)
+
+puts Employees
